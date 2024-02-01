b1d182c8cf04756faf1ef33cb6d964d42e8f5016
Fuzious <manjulkhatter01@gmail.com>
2024-02-01 23:52:00 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>Manjul's Gallery</title>
+        <title>GFSC 2024 Batch All Memories</title>
         <meta
           property="og:image"
           content=""
@@ -59,19 +59,19 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </div>
             {/* <Logo /> */}
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              All time photos
+              GFSC-24: Our Journey Together.
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              All the crazy pictures mahfuz have taken so far...
+              Capturing Unforgettable Moments on the Journey to the End of School Life...
             </p>
-            <a
+{/*             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
               href="https://mahfuz-portfolio.vercel.app/"
               target="_blank"
               rel="noreferrer"
             >
-              Portfolio
-            </a>
+              My Portfolio
+            </a> */}
           </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
